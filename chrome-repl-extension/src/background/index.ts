01959fa9c30b4e4eb420e8c04abe4b14f4435e85
stc <standardtemplateconstruct2022@gmail.com>
2023-01-02 11:37:36 +0200

    first

--- chrome-repl-extension/src/background/index.ts
+++ chrome-repl-extension/src/background/index.ts
@@ -4,6 +4,6 @@ messageInBackground();
 
 // This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
 export function messageInBackground() {
-  console.log('I can run your javascript like any other code in your project');
+  console.log('I <3 ');
   console.log('just do not forget, I cannot render anything !');
 }
